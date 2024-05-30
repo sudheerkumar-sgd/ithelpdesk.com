0dc94ec96f9ed5db93d84a0221eac5ba17dd0115
sudheerkumar-sgd <sudheer.akula@sgd.uaqgov.ae>
2024-05-30 09:33:46 +0400

    #

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -81,8 +81,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "722d9a8d58a76c2e8d6eac27d8b5f6e9
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9913ce415dd2d3a4fd5a5101bf087eab",
-"/": "9913ce415dd2d3a4fd5a5101bf087eab",
+"index.html": "b4bb6ff0ddd807da0433ca634d059fed",
+"/": "b4bb6ff0ddd807da0433ca634d059fed",
 "main.dart.js": "71495b11e46d21cd2293c43d15e3a16b",
 "manifest.json": "a51eedeef89a6c7a82a6cb5320596c4b",
 "version.json": "e053a527cd4a296022bce40d4d1ca5d9"};
